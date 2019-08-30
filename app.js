a590e3d49ecabb390e9495af50168f92c09e2b74
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-29 23:13:10 -0300

    atu

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+function convertToHtml (conteudo){
+    return "<html><body>" + conteudo + "</body></html>"
+}
+app.get('/', function (req, res) {
+   res.send(convertToHtml("Site de Noticias")); 
+});
+
+app.get('/tecnologia', function (req, res) {
+    res.send(convertToHtml("Site de tecnologia")); 
+});
+
+app.listen(3000, function () {
+    console.log('Servidor rodando com Express');
+});
