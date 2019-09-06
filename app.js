fbf34255c01211fe003bbc8cc72e2edab1193873
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-09-05 23:54:04 -0300

    organizando rotas

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 let app = require('./config/server');
 
+let rotaHome = require('./app/routes/home')(app);
+let rotaNoticias = require('./app/routes/noticias')(app);
+let rotaCadastrarNoticia = require('./app/routes/cadastrarNoticia')(app);
+
+
+
 function convertToHtml (conteudo){
     return "<html><body>" + conteudo + "</body></html>"
 }
-app.get('/', function (req, res) {
-    res.render('home/index');
-});
 
-app.get('/formulario_inclusao_noticia', function (req, res) {
-    res.render('admin/form_add_noticia');
-});
 
-app.get('/noticias', function (req, res) {
-    res.render('noticias/noticias');
-});
+
+
 
 app.listen(3000, function () {
     console.log('Servidor rodando com Express:');
