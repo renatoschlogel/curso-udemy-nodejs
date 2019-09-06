bc344678a5570dd44b51e24128f8810145a53568
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-09-06 00:36:09 -0300

    efetuando conexao mysql (docker)

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,20 @@
 module.exports = function(app){
+    
 
     app.get('/noticias', function (req, res) {
-        res.render('noticias/noticias');
+
+        let mysql = require('mysql');
+        let connection = mysql.createConnection({
+            host: 'db',
+            user: 'root',
+            password: '123.456',
+            database: 'portal_noticias'
+        });
+
+        let consulta = 'select * from noticia';
+        connection.query(consulta, function (error, result) {
+            res.send(result);
+        });
+        // res.render('noticias/noticias');
     });
 }
